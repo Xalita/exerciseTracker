b1f329049e907827d9f002f7421f348d3b9bc6ff
xalita <brunofneferreira@gmail.com>
2022-01-06 15:00:03 +0000

    esta

--- server.js
+++ server.js
@@ -1,116 +1,130 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config();
-const mongoose = require("mongoose");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const mongoose = require ('mongoose');
 const db = process.env.MONGO;
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser')
 
-const TRACK = require("./model/exercise");
-const USER = require("./model/user");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const TRACK = require ('./model/exercise');
+const USER = require ('./model/user');
 
-app.use(cors());
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.use (bodyParser.urlencoded({extended:false}));
+app.use (bodyParser.json());
+
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => console.log("Connected"))
-  .catch((e) => console.log(e));
+mongoose.connect (`mongodb+srv://bruno:sarapita88@cluster0.5jn5b.mongodb.net/freeCode?retryWrites=true&w=majority`)
+  .then (()=> console.log('Connected'))
+  .catch ((e)=> console.log (e))
 
-app.post("/api/users", async (req, res) => {
-  const { username } = req.body;
+
+
+app.post('/api/users', async (req,res) => {
+
+  const {username} = req.body;
   let user = await USER.findOne({
-    username: username,
-  });
+    username: username
+  })
 
   if (user) {
     // console.log ('user found: ', user)
-    return res.json(user);
+    return res.json(user)
   } else {
     try {
-      user = new USER({
-        username: username,
+      user = new USER ( {
+        username: username
       });
       // console.log ('user created', user)
       user.save();
       return res.json(user);
-    } catch (e) {
-      console.log("error", e);
     }
-  }
-});
+    catch (e) {
+      console.log ('error',e);
+    }
 
-app.post("/api/users/:_id/exercises", async (req, res) => {
-  const { _id } = req.params;
-  const { description, date, duration } = req.body;
+  }
+
+})
+
+app.post('/api/users/:_id/exercises', async (req,res)=> {
+  const {_id} = req.params;
+  const {description, date, duration} = req.body;
   const newDate = new Date(date).toDateString();
 
   const findId = await USER.findById(_id);
 
+  
   if (!findId) {
-    return res.json({
-      error: "User not Found",
+    return res.json ({
+      error: 'User not Found'
     });
   } else {
-    try {
-      const newExerc = new TRACK({
-        username: findId.username,
-        description: description,
-        duration: duration,
-        date: newDate,
-        userId: findId._id,
-      });
-      newExerc.save();
-      console.log(newExerc);
-      return res.send({
-        username: findId.username,
-        description: description,
-        duration: duration,
-        date: newDate,
-        userId: findId._id,
-      });
-    } catch (e) {
-      console.log("error ,", e);
-    }
+      try {
+        const newExerc = new TRACK ( {
+          username: findId.username,
+          description: description,
+          duration: duration,
+          date: newDate,
+          userId: findId._id
+        });
+        newExerc.save();
+        console.log (newExerc);
+        return res.send({
+          username: findId.username,
+          description: description,
+          duration: duration,
+          date: newDate,
+          userId: findId._id
+        });
+      }
+      catch(e) {
+        console.log ('error ,', e)
+      }
   }
+
 });
 
-app.get("/api/users", async (req, res) => {
-  const users = await USER.find({});
-  console.log(users);
+
+app.get ('/api/users', async (req,res) => {
+
+  const users = await USER.find({})
+  console.log (users);
   res.json(users);
-});
+}); 
 
-app.get("/api/users/:_id/logs", async (req, res) => {
-  const { _id } = req.params;
-  const user = await USER.findById(_id);
-  const idIsFound = await TRACK.find({});
 
-  const getUser = idIsFound.filter((item) => item.userId === _id);
-  console.log(getUser);
-  // console.log (idIsFound.length);
+app.get ('/api/users/:_id/logs', async (req,res)=>{
+    const {_id} = req.params;
+    const user = await USER.findById (_id);
+    const idIsFound = await TRACK.find({});
+
+    const getUser = idIsFound.filter(item => item.userId === _id);
+  console.log (getUser);
+    // console.log (idIsFound.length);
   // console.log ( idIsFound);
   // console.log (typeof user);
   if (!idIsFound) {
-    return res.json("Not found");
-  } else {
-    const { userId, description, duration, date } = getUser;
-    const { username } = user;
+    return res.json ('Not found'); 
+  }else {
+    const {userId, description, duration, date} = getUser;
+    const {username} = user;
     return res.json({
       username,
       userId,
-      logs: getUser,
-    });
+      logs:getUser
+    })
   }
-});
+})
 
-const listener = app.listen(process.env.PORT || 8000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
